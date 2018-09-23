fba5b49c908658abd21dcd536c418961c5e29cb1
Scott W. Davis <zscottywd@msn.com>
2018-09-22 19:00:35 -0700

    initial upload

---
+++ RNNModel.py
@@ -0,0 +1,24 @@
+class RNNModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(RNNModel, self).__init__()
+        # Number of hidden dimensions
+        self.hidden_dim = hidden_dim
+        
+        # Number of hidden layers
+        self.layer_dim = layer_dim
+        
+        # RNN
+        self.rnn = nn.RNN(input_dim, hidden_dim, layer_dim, batch_first=True, 
+                          nonlinearity='relu')
+        
+        # Readout layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+    
+    def forward(self, x):
+        # Initialize hidden state with zeros
+        h0 = Variable(torch.zeros(self.layer_dim, x.size(0), self.hidden_dim))
+            
+        # One time step
+        out, hn = self.rnn(x, h0)
+        out = self.fc(out[:, -1, :]) 
+        return out
